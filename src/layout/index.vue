ebd867b3a23c9afb0eacdc00ecc91007f9c52232
daifazhan <daifzh@163.com>
2022-05-09 16:35:32 +0800

    style: 创建基于 Layout 的基础架构

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,9 +1,44 @@
 <template>
-  <div class="">Layout ⻚⾯</div>
+  <div class="app-wrapper">
+    <!-- 左侧 menu -->
+    <sidebar
+      id="guide-sidebar"
+      class="sidebar-container"
+      :style="{ backgroundColor: variables.menuBg }"
+    /><!-- 为了主题更换，此处使用动态scss -->
+    <div class="main-container">
+      <div class="fixed-header">
+        <!-- 顶部的 navbar -->
+        <navbar />
+      </div>
+      <!-- 内容区 -->
+      <app-main />
+    </div>
+  </div>
 </template>
 
 <script setup>
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import AppMain from './components/AppMain'
+import variables from '@/styles/variables.module.scss'
 import {} from 'vue'
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '~@/styles/mixin.scss'; // webpack
+@import '~@/styles/variables.module.scss';
+.app-wrapper {
+  @include clearfix; // 使用mixin里的clearfix
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  width: calc(100% - #{$sideBarWidth});
+}
+</style>
