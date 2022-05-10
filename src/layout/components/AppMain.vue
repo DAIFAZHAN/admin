e371bad378bcabffd11dda848806032d1312511f
daifazhan <daifzh@163.com>
2022-05-11 05:36:41 +0800

    feat: tagsView 国际化处理

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -6,14 +6,14 @@
 <script setup>
 import { watch } from 'vue'
 import { isTags } from '@/utils/tags'
-import { generateTitle } from '@/utils/i18n'
+import { generateTitle, watchSwitchLang } from '@/utils/i18n'
 import { useRoute } from 'vue-router'
 import { useStore } from 'vuex'
 const route = useRoute()
 /**
-* ⽣成 title
-*/
-const getTitle = route => {
+ * ⽣成 title
+ */
+const getTitle = (route) => {
   let title = ''
   if (!route.meta) {
     // 处理⽆ meta 的路由
@@ -25,8 +25,8 @@ const getTitle = route => {
   return title
 }
 /**
-* 监听路由变化
-*/
+ * 监听路由变化
+ */
 const store = useStore()
 watch(
   route,
@@ -47,6 +47,21 @@ watch(
     immediate: true
   }
 )
+
+/**
+ * 国际化 tags
+ */
+watchSwitchLang(() => {
+  store.getters.tagsViewList.forEach((route, index) => {
+    store.commit('app/changeTagsView', {
+      index,
+      tag: {
+        ...route,
+        title: getTitle(route)
+      }
+    })
+  })
+})
 </script>
 <style lang="scss" scoped>
 .app-main {
