4a9be227189d3efeb9ec184456ca411c3f87656d
daifazhan <daifzh@163.com>
2022-05-10 19:35:22 +0800

    feat: 创建 ThemeSelect 组件

---
+++ src/components/ThemeSelect/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- 主题图标
+ v-bind：https://v3.cn.vuejs.org/api/instance-properties.html#attrs
+ v-bind="$attrs" 父组件给他指定的属性会直接绑定到该元素
+ -->
+  <el-dropdown
+    v-bind="$attrs"
+    trigger="click"
+    class="theme"
+    @command="handleSetTheme"
+  >
+    <div>
+      <el-tooltip :content="$t('msg.navBar.themeChange')">
+        <svg-icon icon="change-theme" />
+      </el-tooltip>
+    </div>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item command="color">
+          {{ $t('msg.theme.themeColorChange') }}
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+  <!-- 展示弹出层 -->
+  <div></div>
+</template>
+<script setup>
+const handleSetTheme = (command) => {}
+</script>
+<style lang="scss" scoped></style>
