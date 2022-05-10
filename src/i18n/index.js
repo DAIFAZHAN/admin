c20e8fa3346d1c67d0d44b0cad02798a85a7a893
daifazhan <daifzh@163.com>
2022-05-10 17:51:42 +0800

    style: 国际化缓存处理

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,6 +1,7 @@
 import { createI18n } from 'vue-i18n'
 import mZhLocale from './lang/zh'
 import mEnLocale from './lang/en'
+import store from '@/store'
 
 const messages = {
   en: {
@@ -14,14 +15,19 @@ const messages = {
     }
   }
 }
-const locale = 'zh'
+/**
+ * 返回当前 lang
+ */
+function getLanguage() {
+  return store && store.getters && store.getters.language
+}
 
 const i18n = createI18n({
   // 使⽤ Composition API 模式，则需要将其设置为false
   legacy: false,
   // 全局注⼊ $t 函数
   globalInjection: true,
-  locale,
+  locale: getLanguage(),
   messages
 })
 
