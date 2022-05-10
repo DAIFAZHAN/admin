d035077758aa0817bfce4c1b7b74fa1ad17b4dfb
daifazhan <daifzh@163.com>
2022-05-11 02:58:55 +0800

    feat: screenfull库实现全屏

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -3,6 +3,7 @@
     <hamburger class="hamburger-container" />
     <breadcrumb class="breadcrumb-container" />
     <div class="right-menu">
+      <screenfull class="right-menu-item hover-effect" />
       <theme-picker class="right-menu-item hover-effect"></theme-picker>
       <lang-select class="right-menu-item hover-effect" />
       <!-- 头像 -->
@@ -41,6 +42,7 @@ import Hamburger from '@/components/Hamburger'
 import Breadcrumb from '@/components/Breadcrumb'
 import LangSelect from '@/components/LangSelect'
 import ThemePicker from '@/components/ThemeSelect/index'
+import Screenfull from '@/components/Screenfull'
 
 const store = useStore()
 const logout = () => {
