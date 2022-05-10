f5124c0f32e2b067a17d9cfd4eaab92e98c1ae7e
daifazhan <daifzh@163.com>
2022-05-11 04:38:01 +0800

    feat: 搜索栏关闭处理、国际化处理

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -1,41 +1,24 @@
 <template>
   <div :class="{ show: isShow }" class="header-search">
-    <svg-icon
-      class-name="search-icon"
-      icon="search"
-      @click.stop="onShowClick"
-    />
-    <el-select
-      ref="headerSearchSelectRef"
-      class="header-search-select"
-      v-model="search"
-      filterable
-      default-first-option
-      remote
-      placeholder="Search"
-      :remote-method="querySearch"
-      @change="onSelectChange"
-    >
-      <el-option
-        v-for="option in searchOptions"
-        :key="option.item.path"
-        :label="option.item.title.join(' > ')"
-        :value="option.item"
-      ></el-option>
+    <svg-icon class-name="search-icon" icon="search" @click.stop="onShowClick" />
+    <el-select ref="headerSearchSelectRef" class="header-search-select" v-model="search" filterable default-first-option
+      remote placeholder="Search" :remote-method="querySearch" @change="onSelectChange">
+      <el-option v-for="option in searchOptions" :key="option.item.path" :label="option.item.title.join(' > ')"
+        :value="option.item"></el-option>
     </el-select>
   </div>
 </template>
 <script setup>
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 import { generateRoutes } from './FuseData'
 import Fuse from 'fuse.js'
 import { filterRouters } from '@/utils/route'
 import { useRouter } from 'vue-router'
+import { watchSwitchLang } from '@/utils/i18n'
 
 // 检索数据源
-// 检索数据源
 const router = useRouter()
-const searchPool = computed(() => {
+let searchPool = computed(() => {
   const filterRoutes = filterRouters(router.getRoutes())
   return generateRoutes(filterRoutes)
 })
@@ -43,28 +26,54 @@ const searchPool = computed(() => {
 /**
  * 搜索库相关
  */
-const fuse = new Fuse(searchPool.value, {
-  // 是否按优先级进⾏排序
-  shouldSort: true,
-  // 匹配⻓度超过这个值的才会被认为是匹配的
-  minMatchCharLength: 1,
-  // 将被搜索的键列表。 这⽀持嵌套路径、加权搜索、在字符串和对象数组中搜索。
-  // name：搜索的键
-  // weight：对应的权重
-  keys: [
-    {
-      name: 'title',
-      weight: 0.7
-    },
-    {
-      name: 'path',
-      weight: 0.3
-    }
-  ]
-})
+let fuse
+const initFuse = searchPool => {
+  fuse = new Fuse(searchPool, {
+    // 是否按优先级进⾏排序
+    shouldSort: true,
+    // 匹配⻓度超过这个值的才会被认为是匹配的
+    minMatchCharLength: 1,
+    // 将被搜索的键列表。 这⽀持嵌套路径、加权搜索、在字符串和对象数组中搜索。
+    // name：搜索的键
+    // weight：对应的权重
+    keys: [
+      {
+        name: 'title',
+        weight: 0.7
+      },
+      {
+        name: 'path',
+        weight: 0.3
+      }
+    ]
+  })
+}
+initFuse(searchPool.value)
 
 // 控制 search 显示
 const isShow = ref(false)
+/**
+ * 关闭 search 的处理事件
+ */
+const onClose = () => {
+  headerSearchSelectRef.value.blur() // 失去焦点
+  isShow.value = false
+  searchOptions.value = []
+}
+/**
+ * 监听 search 打开，处理 close 事件
+ */
+watch(isShow, val => {
+  if (val) {
+    headerSearchSelectRef.value.focus()
+    // 搜索框开启时，点击其他触发事件
+    document.body.addEventListener('click', onClose)
+  } else {
+    // 移除事件
+    document.body.removeEventListener('click', onClose)
+  }
+})
+
 // el-select 实例
 const headerSearchSelectRef = ref(null)
 const onShowClick = () => {
@@ -83,19 +92,31 @@ const querySearch = query => {
     searchOptions.value = []
   }
 }
+
 // 选中回调
 const onSelectChange = val => {
   router.push(val.path)
 }
+
+// 处理国际化
+watchSwitchLang(() => {
+  searchPool = computed(() => {
+    const filterRoutes = filterRouters(router.getRoutes())
+    return generateRoutes(filterRoutes)
+  })
+  initFuse(searchPool.value)
+})
 </script>
 <style lang="scss" scoped>
 .header-search {
   font-size: 0 !important;
+
   .search-icon {
     cursor: pointer;
     font-size: 18px;
     vertical-align: middle;
   }
+
   .header-search-select {
     font-size: 18px;
     transition: width 0.2s;
@@ -105,6 +126,7 @@ const onSelectChange = val => {
     border-radius: 0;
     display: inline-block;
     vertical-align: middle;
+
     ::v-deep .el-input__inner {
       border-radius: 0;
       border: 0;
@@ -115,6 +137,7 @@ const onSelectChange = val => {
       vertical-align: middle;
     }
   }
+
   &.show {
     .header-search-select {
       width: 210px;
