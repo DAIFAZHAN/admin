c4e1720dd45653236a0b3ccefa8343319954c6a0
daifazhan <daifzh@163.com>
2022-05-11 14:41:44 +0800

    feat: 处理基于路由的动态过渡

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="app-main">
-    <router-view></router-view>
+    <!-- 带有切换动画，并且具备组件缓存的 -->
+    <router-view v-slot="{ Component, route }">
+      <transition name="fade-transform" mode="out-in">
+        <keep-alive>
+          <component :is="Component" :key="route.path" />
+        </keep-alive>
+      </transition>
+    </router-view>
   </div>
 </template>
+
 <script setup>
 import { watch } from 'vue'
 import { isTags } from '@/utils/tags'
@@ -65,11 +73,11 @@ watchSwitchLang(() => {
 </script>
 <style lang="scss" scoped>
 .app-main {
-  min-height: calc(100vh - 50px);
+  min-height: calc(100vh - 50px - 43px);
   width: 100%;
   position: relative;
   overflow: hidden;
-  padding: 61px 20px 20px 20px;
+  padding: 104px 20px 20px 20px;
   box-sizing: border-box;
 }
 </style>
